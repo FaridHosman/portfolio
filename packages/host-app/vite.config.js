6e065c246332ef64c96423a787390e21ae3dbf51
Farid Hosman <farid.hosman@gmail.com>
2025-02-23 21:51:16 -0500

    Updates targe of host viteconfig

--- packages/host-app/vite.config.js
+++ packages/host-app/vite.config.js
@@ -1,17 +1,21 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import federation from '@originjs/vite-plugin-federation';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: 'hostApp',
+      name: "hostApp",
       remotes: {
-        remoteApp: 'http://localhost:5001/assets/remoteEntry.js',
+        remoteApp: "http://localhost:5001/assets/remoteEntry.js",
       },
-      shared: ['react', 'react-dom'],
+      shared: ["react", "react-dom"],
     }),
   ],
+
+  build: {
+    target: "ES2022",
+  },
 });
